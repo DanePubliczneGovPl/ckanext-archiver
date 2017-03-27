37407f11adbd264064abe91bdccf8dd88a9450eb
krzysztofmadejski <krzysztof@madejscy.pl>
2017-03-27 13:46:27 +0200

    Mock should have empty dict as headers #282

--- ckanext/archiver/tasks.py
+++ ckanext/archiver/tasks.py
@@ -276,7 +276,7 @@ def _update_resource(resource_id, queue, log):
             return
 
         mimetype = None
-        headers = None
+        headers = {}
         content_type, content_encoding = mimetypes.guess_type(url)
         if content_type:
             mimetype = _clean_content_type(content_type)
